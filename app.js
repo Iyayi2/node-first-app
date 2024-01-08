9b707c6d7a4c4c0662c0f6d4d453e907aaaf1455
Iyayi Roland <iyayiroland@gmail.com>
2024-01-08 13:22:47 +0100

    shopping app

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 const path = require('path');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
-const bodyParser = require('body-parser')
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: false}));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminData.routes);
+app.use((req, res, next) => {
+  User.findById('659975ddffb31c5f8c1d2513')
+    .then(user => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
+app.use(errorController.get404);
 
-app.listen(3000);
+mongoConnect(() => {
+  app.listen(3000);
+});
