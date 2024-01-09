6811624d40cb93de030992f372e3a2a21357224d
Iyayi Roland <iyayiroland@gmail.com>
2024-01-09 09:32:33 +0100

    added session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,26 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
   console.log(req.get('Cookie'));
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: req.isLoggedIn
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true');
-  res.redirect('/');
+  User.findById('659c6a4466510feace6913f2')
+  .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(() => {
+    res.redirect('/');
+  });
 };
