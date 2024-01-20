fcfdc668658eec7dfbf59ea99ca4b1f2836f5c2a
Iyayi Roland <iyayiroland@gmail.com>
2024-01-20 13:24:05 +0100

    added advance authentication

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 const express = require('express');
 
 const authController = require('../controllers/auth');
@@ -7,9 +5,21 @@ const authController = require('../controllers/auth');
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
+
 router.get('/signup', authController.getSignup);
+
 router.post('/login', authController.postLogin);
+
 router.post('/signup', authController.postSignup);
+
 router.post('/logout', authController.postLogout);
+
+router.get('/reset', authController.getReset);
+
+router.post('/reset', authController.postReset);
+
+router.get('/reset/:token', authController.getNewPassword);
+
+router.post('/new-password', authController.postNewPassword);
 
 module.exports = router;
